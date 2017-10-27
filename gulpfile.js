45853be1a61acc5ac2fcfd703d917d0b575447ef
Anthony Carroll <busyuqboy@yahoo.com>
2017-10-27 07:22:54 -0400

    Create assets folder. update browser-sync to include html

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,22 @@
 var gulp = require('gulp');  
-var gulp = require('gulp');  
 var sass = require('gulp-sass');  
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function () {  
-    gulp.src('scss/styles.scss')
+    gulp.src('assets/scss/**.scss')
         .pipe(sass({includePaths: ['scss']}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('browser-sync', function() {  
-    browserSync.init(["css/*.css", "js/*.js"], {
+    browserSync.init(["**/*.html", "assets/css/*.css", "assets/js/*.js"], {
         server: {
             baseDir: "./"
-        },
-	open: false,
-	port: 3000
+        }
     });
 });
 
 gulp.task('default', ['sass', 'browser-sync'], function () {  
     gulp.watch("scss/*.scss", ['sass']);
+	gulp.watch("**/*.html").on('change', browserSync.reload);
 });
